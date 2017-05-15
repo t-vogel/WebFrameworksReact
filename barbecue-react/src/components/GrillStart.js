b710b7af642c6fcbda76df78ecb0c06cc1363978
Florian-LM <florian-laurin@web.de>
2017-05-15 11:54:47 +0200

    Ordnerstrucktur festgelegt mit imports

---
+++ barbecue-react/src/components/GrillStart.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class GrillStart extends Component {
+    render() {
+        return (
+            <div className="GrillStart">
+                <div className="GrillStart-header">
+                    <h2>Welcome to GrillStart</h2>
+                </div>
+                <p className="Grill-intro">
+                    Hat der scheiss jetzt funktioniert?
+                </p>
+            </div>
+        );
+    }
+}
+
+export default GrillStart;
