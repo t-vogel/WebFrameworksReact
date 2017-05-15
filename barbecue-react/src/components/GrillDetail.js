78cb1de832fe6a656a76a1325f96e48113c5fb2e
J-Wolf <j.t.wolf@hotmail.de>
2017-05-15 12:56:03 +0200

    Grill Detail Component

--- barbecue-react/src/components/GrillDetail.js
+++ barbecue-react/src/components/GrillDetail.js
@@ -1,3 +1,23 @@
-/**
- * Created by Flo on 15.05.2017.
- */
+import React, { Component } from 'react';
+import Layout from './Layout';
+
+class GrillDetail extends Component
+{
+    render ()
+    {
+        return (
+        <div className="GrillDetail">
+            <h1 className="GrillDetail">HIER GIBTS GEILE GRILLINFOS</h1>
+                <ul>
+                    <li>Standort</li>
+                    <li>Besitzer</li>
+                    <li>Mietpreis</li>
+                    <li>Grillart</li>
+                </ul>
+        </div>
+     );
+    }
+
+}
+
+export default GrillDetail;
