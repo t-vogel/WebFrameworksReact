509162094bc552b7cc81f2ab8203fef71cc1d2da
t-vogel <tovo@web.de>
2017-05-19 13:12:02 +0200

    tobi

--- barbecue-react/src/index.js
+++ barbecue-react/src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import GrillStart from "./components/GrillStart";
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+import GrillBooking from "./components/GrillBooking";
 
 ReactDOM.render(
-  <GrillStart />,
+    <Router>
+        <div>
+
+            <Route exact path="/" component={GrillStart}/>
+            <Route exact path="/add" component={GrillBooking}/>
+
+        </div>
+    </Router>
+,
   document.getElementById('root')
 );
