9ed26a48f90d2869d65fe061a248cb3e27c9f47b
Florian-LM <florian-laurin@web.de>
2017-05-19 11:19:22 +0200

    Grill Start funktioniert

--- barbecue-react/src/components/GrillStart.js
+++ barbecue-react/src/components/GrillStart.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import Layout from './Layout';
+import {Map, Marker, GoogleApiWrapper }from 'google-maps-react';
 
 class GrillStart extends Component {
     render() {
         return (
 
+<Layout>
 
             <div className="GrillStart">
 
-                <p>Heisse Grills in deiner nähe</p>
+                <h1>Heisse Grills in deiner Nähe</h1>
+
+                <Map style={{height:"300px"}} google={this.props.google} zoom={14}>
+
+                </Map>
+
                 <p className="Grill-intro">
                 </p>
             </div>
-
+    <p style={{paddingTop:"350px"}}>Liste mit Grills mit deiner Nähe:</p>
+    <li> Grill Dummy </li>
+</Layout>
         );
     }
 }
 
-export default GrillStart;
+export default GoogleApiWrapper({
+    apiKey:'AIzaSyBVD27r1AY8j4VH19kZoOIQRkjLuqoXelY'
+
+})(GrillStart);
